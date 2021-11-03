ba105efa0e5c60a8aebc35438965cec326d724e2
sohagraha <sohagrahabd@gmail.com>
2021-11-04 00:57:07 +0600

    connected

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fj33t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+// console.log(uri);
+
+async function run() {
+    try {
+        await client.connect()
+
+        const database = client.db('doctorappointment');
+        const collection = database.collection('demo');
+
+        console.log('SUCCESSFULLY');
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('Hello Doctor Portal!')
