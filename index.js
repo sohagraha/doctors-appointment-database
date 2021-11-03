1faf63dfc2584d2948eee320d20385ae6a631d19
sohagraha <sohagrahabd@gmail.com>
2021-11-04 00:37:07 +0600

    addeded uri

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient } = require('mongodb');
+
 const port = process.env.PORT || 5000
 
 // middleware 
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fj33t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
 
 app.get('/', (req, res) => {
     res.send('Hello Doctor Portal!')
